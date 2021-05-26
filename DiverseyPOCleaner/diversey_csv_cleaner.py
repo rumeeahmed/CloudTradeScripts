f8b842bb37e1c6a88feef9eda54b524c05b0134a
Rumee Ahmed <rumeeahmad@gmail.com>
2021-05-26 15:22:21 +0100

    Reworked the script to be less Diversey specific and more general so it can be used for other customers.

--- DiverseyPOCleaner/diversey_csv_cleaner.py
+++ DiverseyPOCleaner/diversey_csv_cleaner.py
@@ -8,20 +8,26 @@ class DiverseyPOCleaner:
     """
     Object that cleans Diversey's PO feed.
     """
-
-    def __init__(self, directory_path: str, clean_write_path: str, unclean_write_path: str, archive_path: str):
+    def __init__(
+            self, directory_path: str,
+            clean_write_path: str,
+            unclean_write_path: str,
+            archive_path: str,
+            row_length: int
+    ):
         """
-
         :param directory_path: The directory that contains the csv.
         :param clean_write_path: The directory to write the cleaned csv file to.
         :param unclean_write_path: The directory to write the uncleaned csv file to.
         :param archive_path: The directory to move the original file to.
+        :param archive_path: The number of expected rows to clean the file by.
         """
 
         self.directory_path = directory_path
         self.clean_write_path = clean_write_path
         self.unclean_write_path = unclean_write_path
         self.archive_path = archive_path
+        self.row_length = row_length
 
     def _clean(self):
         """
@@ -45,10 +51,12 @@ class DiverseyPOCleaner:
                 while '' in row:
                     row.remove('')
 
-                if len(row) == 15:
+                if len(row) == int(row_length):
                     self._rows_cleaned.append(row)
                 else:
                     self._rows_not_cleaned.append(row)
+
+        self._move_original_file()
 
     def _write_cleaned_csv(self):
         """
@@ -57,7 +65,7 @@ class DiverseyPOCleaner:
         """
 
         self._now = datetime.now().strftime('%d %m %Y')
-        filename = f'Diversey PO Feed Cleaned {self._now}.csv'
+        filename = f"{self._original_file_name.strip('.csv')}_cleaned_{self._now}.csv"
 
         with open(f'{self.clean_write_path}/{filename}', 'w', newline='', encoding="utf-8") as csv_file:
             csv_writer = csv.writer(csv_file, delimiter=',')
@@ -70,7 +78,7 @@ class DiverseyPOCleaner:
         :return: A csv containing the rows that need to be removed from the PO feed.
         """
 
-        file_name = f'Diversey PO Feed Lines Removed {self._now}.csv'
+        file_name = f"{self._original_file_name.strip('.csv')}_uncleaned_{self._now}.csv"
         with open(f'{self.unclean_write_path}/{file_name}', 'w', newline='', encoding="utf-8") as csv_file:
             csv_writer = csv.writer(csv_file, delimiter=',')
             for row in self._rows_not_cleaned:
@@ -91,13 +99,14 @@ class DiverseyPOCleaner:
         self._clean()
         self._write_cleaned_csv()
         self._write_uncleaned_csv()
-        self._move_original_file()
 
 
-directory_path = r'D:\FTP\Diversey\Prod\AribaPOLookup'
-clean_write_path = r'D:\FTP\Diversey\Prod\AribaPOLookup\Cleaned Files'
-unclean_write_path = r'D:\FTP\Diversey\Prod\AribaPOLookup\Rejections'
-archive_path = r'D:\FTP\Diversey\Prod\AribaPOLookup\Archive'
+directory_path = r'/Users/rumeeahmed/Documents/CloudTradeScripts/DiverseyPOCleaner'
+clean_write_path = r'/Users/rumeeahmed/Documents/CloudTradeScripts/DiverseyPOCleaner/cleaned'
+unclean_write_path = r'/Users/rumeeahmed/Documents/CloudTradeScripts/DiverseyPOCleaner/unclean'
+archive_path = r'/Users/rumeeahmed/Documents/CloudTradeScripts/DiverseyPOCleaner/archive'
+row_length = 15
 
-po = DiverseyPOCleaner(directory_path, clean_write_path, unclean_write_path, archive_path)
+po = DiverseyPOCleaner(directory_path, clean_write_path, unclean_write_path, archive_path, row_length)
 po.process_csv()
+
--- DiverseyPOCleaner/readme.md
+++ DiverseyPOCleaner/readme.md
@@ -6,13 +6,18 @@ escaped correctly this creating more columns than necessary.
 This file will be executed on a daily basis on the `Diversey` user to clean their file. Then the existing Powershell
 script will pull the new cleaned file to be uploaded to Gratabase.
 
+
+---
+
+
 ## Usage
-To use the script an object of `DiverseyPOCleaner` must be instatiated with four parameters:
+To use the script, an object of `DiverseyPOCleaner` must be instantiated with five parameters:
 ```python
 directory_path = r'D:\FTP\Diversey\Prod\AribaPOLookup'
 clean_write_path = r'D:\FTP\Diversey\Prod\AribaPOLookup\Cleaned Files'
 unclean_write_path = r'D:\FTP\Diversey\Prod\AribaPOLookup\Rejections'
 archive_path = r'D:\FTP\Diversey\Prod\AribaPOLookup\Archive'
+row_length = 15
 
 po = DiverseyPOCleaner(directory_path, clean_write_path, unclean_write_path, archive_path)
 ```
@@ -23,6 +28,9 @@ The `clean_write_path` represents the location to write the new cleaned file.
 The `unclean_write_path` represents the location to write the stripped out rows from the original feed.
 
 The `archive_path` represents the location to the original po feed file to.
+
+The `row_length` represents the number of expected rows to clean the po feed by. For example if the rows are supposed
+to be 15 in length but due to extra commas the actual length of the rows are 17.
 
 To execute the program use the following:
 ```python
