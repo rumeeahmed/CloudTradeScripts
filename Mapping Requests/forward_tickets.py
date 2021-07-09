5b4c9d48f978e9b09f973a66b22e3458df43c656
RumeeAhmed <rumee.ahmed@cloud-trade.com>
2021-07-09 09:10:08 +0100

    every posted filed will now move into a different folder

--- Mapping Requests/forward_tickets.py
+++ Mapping Requests/forward_tickets.py
@@ -11,8 +11,8 @@ for pdf in pdfs:
     if pdf.endswith('.pdf'):
         zendesk = ZendeskForwarder('rumee.ahmed@cloud-trade.com')
         response = zendesk.send_mapping(customer, f'{file_path}\\{pdf}')
+        shutil.move(f'{file_path}\\{pdf}', move_path)
         remaining_api_calls = int(response[1]['X-Rate-Limit-Remaining'])
         if remaining_api_calls < 200:
             print('Exceeding the API rate limit for this program')
-            shutil.move(f'{file_path}\\{pdf}', move_path)
             break
