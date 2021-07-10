fe3ad2657f08441f50f529b5623f6c9651f96dda
Rumee Ahmed <rumeeahmad@gmail.com>
2021-07-10 14:14:33 +0100

    Removed print statements

--- Zendesk/daily_intervention.py
+++ Zendesk/daily_intervention.py
@@ -3,14 +3,13 @@ import time
 
 zendesk = CloudTradeZendesk('rumee.ahmed@cloud-trade.com')
 response, total_count = zendesk.submit_intervention_tickets()
-print(total_count)
 
-calls = total_count/100
-for count in range(round(calls)):
-    response, total_count = zendesk.submit_intervention_tickets()
+for count in range(total_count):
+    response, total_count2 = zendesk.submit_intervention_tickets()
     remaining_api_calls = int(response.headers['X-Rate-Limit-Remaining'])
     time.sleep(2)
-    print(remaining_api_calls)
     if remaining_api_calls < 350:
         print('Exceeding the API rate limit for this program, waiting 30 seconds.')
         time.sleep(30)
+    if total_count2 == 0:
+        break
