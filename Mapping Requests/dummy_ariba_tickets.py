5cfe927030a410aa8965e990066c88844fc028fd
Rumee Ahmed <rumeeahmad@gmail.com>
2021-07-10 10:13:34 +0100

    removed old files

--- Mapping Requests/dummy_ariba_tickets.py
+++
@@ -1,5 +0,0 @@
-from cloudtrade_zendesk import CloudTradeZendesk
-
-zendesk = CloudTradeZendesk('rumee.ahmed@cloud-trade.com')
-zendesk.submit_junk_ariba_tickets('subject:"Your Service Requests have been closed"')
-zendesk.submit_junk_ariba_tickets('subject:"Your Ariba Request was Unable to be Processed"')
